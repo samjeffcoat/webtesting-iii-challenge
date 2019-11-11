d847348a185909ac9e5e568b9d4883cc50d09c42
Sam <Jeffcoat>
2019-11-11 16:08:09 -0600

    Adds test for Red LED light

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,14 @@
 // Test away!
+import React from 'react';
+import {render, fireEvent}from "@testing-library/react";
+import Display from './Display';
+
+test("testing if its closed or locked, light should be red", () => {
+    const {getByText}= render(<Display closed = {true}locked= {true}/>)
+    const closed = getByText(/closed/i);
+    const locked = getByText(/locked/i);
+    expect(locked.classList).toContain("red-led");
+    expect(closed.classList).toContain("red-led");
+        
+});
+test("test ")
