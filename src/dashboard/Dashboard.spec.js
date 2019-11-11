79004da8e787eb4883c76269f95cb2615d515a26
Sam <Jeffcoat>
2019-11-11 14:58:12 -0600

    Tests written to show controls and display on our Dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -7,3 +7,9 @@ import  Dashboard from './Dashboard';
 test("it renders correctly", () => {
     expect(render(<Dashboard />)).toMatchSnapshot();
 })
+
+test( "it renders display and controls", () => {
+    render(<Dashboard />)
+    expect(document.querySelector('.controls')).toBeTruthy();
+    expect(document.querySelector('.display')).toBeTruthy();
+})
