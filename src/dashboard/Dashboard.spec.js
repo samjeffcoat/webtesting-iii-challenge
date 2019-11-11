18c4f0167fc589ac9073e87fe2aa1f876b75f58d
Sam <Jeffcoat>
2019-11-11 14:40:55 -0600

    Project initialized and first test written

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,9 @@
 // Test away
+import React from 'react';
+import { render, fireEvent} from 'react-testing-library';
+import  Dashboard from './Dashboard';
+
+
+test("it renders correctly", () => {
+    expect(render(<Dashboard />)).toMatchSnapshot();
+})
