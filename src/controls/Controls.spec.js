222b5a1ab7ea7124f42057e51227b544c69b2b05
Sam <Jeffcoat>
2019-11-11 15:37:41 -0600

    lock button function written

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,16 @@
 // Test away!
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react';
+import Controls from './Controls';
+
+describe("lock button", () => {
+    it("should not work while gate is open", () => {
+        const toggleLocked = jest.fn();
+        const {getByText} = render(
+            <Controls toggleLocked= {toggleLocked} closed= {false} locked = {false}/>
+        )
+        const button = getByText(/lock gate/i);
+        fireEvent.click(button);
+        expect(toggleLocked).toBeCalledTimes(0);
+    })
+})
